acd6a251193a705aee3f9f4370247e4cd888a36e
wass91 <wass91@hotmail.es>
2015-05-25 21:34:57 -0500

    CRUD para EPS e IPS agregado

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :ips
+  resources :ips
+  resources :ips
+  resources :eps
   get 'ips/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
