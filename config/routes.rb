964e6608ebf9feb7e87aab3c6572f6779620fc6f
Andrea Sicacha <assa920831@gmail.com>
2015-05-27 06:27:08 -0500

    menu

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :ips
   resources :eps
   get 'ips/index'
-
+  root to: "ips#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
